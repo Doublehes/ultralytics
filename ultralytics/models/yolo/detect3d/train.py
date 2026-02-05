bb5cc833506a8affecb0e85b98ec692abfc886f3
heshuang <heshuang>
2026-02-05 18:39:24 +0800

    det3d shuffle

--- ultralytics/models/yolo/detect3d/train.py
+++ ultralytics/models/yolo/detect3d/train.py
@@ -48,9 +48,9 @@ class Detection3DTrainer(BaseTrainer):
         with torch_distributed_zero_first(rank):  # init dataset *.cache only once if DDP
             dataset = self.build_dataset(dataset_path, mode, batch_size)
         shuffle = mode == "train"
-        if getattr(dataset, "rect", False) and shuffle:
-            LOGGER.warning("WARNING ⚠️ 'rect=True' is incompatible with DataLoader shuffle, setting shuffle=False")
-            shuffle = False
+        # if getattr(dataset, "rect", False) and shuffle:
+        #     LOGGER.warning("WARNING ⚠️ 'rect=True' is incompatible with DataLoader shuffle, setting shuffle=False")
+        #     shuffle = False
         workers = self.args.workers if mode == "train" else self.args.workers * 2
         return build_dataloader(dataset, batch_size, workers, shuffle, rank)  # return dataloader
 
